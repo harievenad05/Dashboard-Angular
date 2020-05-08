b8843823fa32ac1b402743690579c9034d2329cc
harievenad <harievenad@gmail.com>
2020-05-08 15:16:16 +0300

    Server status, color and title toggle method added

--- src/app/server/server/server.component.ts
+++ src/app/server/server/server.component.ts
@@ -8,8 +8,28 @@ import { Server } from '../../shared/server';
 })
 export class ServerComponent implements OnInit {
   @Input() serverInput: Server;
+  color: string;
+  buttonText: string;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.setServerAction(this.serverInput.isOnline);
+  }
+
+  setServerAction(onlineStatus: boolean) {
+    if (onlineStatus) {
+      this.serverInput.isOnline = true;
+      this.color = '#66BB6A';
+      this.buttonText = 'Shut Down';
+    } else {
+      this.serverInput.isOnline = false;
+      this.color = '#FF6B6B';
+      this.buttonText = 'Start';
+    }
+  }
+
+  toggleStatus(status: boolean) {
+    this.setServerAction(!status);
+  }
 }
